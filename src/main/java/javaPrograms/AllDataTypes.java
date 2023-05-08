e6b4fd904c66f2951b56ebbeb4bb833d0873f659
est0034 <est0034@DESKTOP-SG29SBQ>
2023-05-08 19:28:33 +0530

    Class, Method - Create and Run

--- src/main/java/javaPrograms/AllDataTypes.java
+++ src/main/java/javaPrograms/AllDataTypes.java
@@ -1,40 +1,33 @@
 package javaPrograms;
 
+//added a method is called "Action"
 public class AllDataTypes {
-			
-	public void DataTypes() {
-			
-	//integer
-	int i1 = 10;
-	int i2 = 30;
-	int i3 = 40;
-	int i4 = -1;
-	int i5 = 0;
 
-	System.out.println(a);
 	
-	//float
-	float f1 = 2.4545f;
-	float f2 = 20.66f;
-	float f3 = 100f;
-	float f4 = 0;
-	float f5 = -3.14f;
-	//character should return within single quotes
-	char c = 'A';
-	char c1 = 'Q';
-	char c2 = '1';
-	//double
-	double d1 = 256.54;
-	double d2 = 67.04;
-	double d3 = 256;
-	//boolean
-	boolean b1 = true;
-	boolean b2 = false;
-	//String is a class, not a data type
-	String s1 = "Selenium";
-	String s2 = "Programming is very easy";
-	String s3 = "12800";
-	String s4 = "12.55";
-
+	public static void main(String[] args) {
+		//added a informations are called "variables"
+		//Primitive datatypes boolean,byte,char,short,int,long,float,double
+		System.out.println("Welcome to the Basic core Java");
+		int carModelNumber = 776045;
+		System.out.println("Model Number: "+carModelNumber);
+		short seatingCapacity = 6;
+		System.out.println("Seats: "+seatingCapacity);
+		long chasisNumber = 562140000546446L;
+		System.out.println("Engine Chasis Number:" +chasisNumber);
+		float fuelTankCapacity = 25.35F;
+		System.out.println("Fuel Tank capacity: "+fuelTankCapacity);
+		double engineCapacity = 5154.256645615149654;
+		System.out.println("Engine Capacity: "+engineCapacity);
+		char varient = 'D';
+		System.out.println("Varient: "+varient);
+		boolean ispunctured = false;
+		System.out.println("Punctured: "+ispunctured);
+		//Derived datatype or Non Primitive datatype
+		String carName = "Skoda 8 Series";
+		System.out.println("Car Brand: "+carName);
+		//Using multiple Object with same class name
+		MultipleClassCall food = new MultipleClassCall();
+		food.Beavarages();
+				
+	}
 }
- 	}
--- src/main/java/javaPrograms/BasicPrinting.java
+++ src/main/java/javaPrograms/BasicPrinting.java
@@ -4,21 +4,21 @@ public class BasicPrinting {
 
 	public void Car() {
 		System.out.println("BMW series 6");
+		System.out.println("Manual into Automation Tester!");
+		//Error output message
+		System.err.println("Employer in the EST");
 	}
 	public static void main(String[] args) {
 		
 		//Created object to call the method, multiple methods & multiple classes
 		BasicPrinting model = new BasicPrinting();
 		model.Car();
+		//Created classes MultipleClassCall.java
 		MultipleClassCall food = new MultipleClassCall();
 		food.Beavarages();
 		food.Transport();
-		System.out.println("Manual into Automation Tester!");
 		
-		//Error output message
-		System.err.println("Employer in the EST");
-		//All data types in the objects
-		AllDataTypes ADT = new AllDataTypes();
+		
 		
 	}
 
